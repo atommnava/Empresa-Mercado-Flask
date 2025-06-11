2063725d21caca8d9a1933f59096599d5690d6a7
atommnava <atommunoznava@gmail.com>
2025-06-11 15:06:42 -0600

    Enviar datos a las plantillas

--- mercado.py
+++ mercado.py
@@ -1,17 +1,20 @@
-from enum import nonmember
+from flask import Flask, render_template
 
-from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h2>Hello Atom!</h2>"
+@app.route("/hogar")
+def home():
+    return render_template('home.html')
 
 @app.route("/about/<usuario>")
 def about_page(usuario):
     return f"<h1>Esta es la pagina ABOUT de {usuario}</h1>"
 
+@app.route("/mercado")
+def market_page():
+    return render_template('market.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
