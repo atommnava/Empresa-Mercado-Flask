878f2fed629b8956df587c4f2cd19dff014392e5
atommnava <atommunoznava@gmail.com>
2025-06-12 13:08:18 -0600

    Modelos y bases de datos

--- mercado.py
+++ mercado.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    name = db.Column(db.String(length=32))
+    name = db.Column(db.String())
 
 @app.route("/")
 @app.route("/hogar")
