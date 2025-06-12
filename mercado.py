c50dc9f4d23e96eb911470a236295d4ffd6a177c
atommnava <atommunoznava@gmail.com>
2025-06-12 13:19:20 -0600

    Modelos y bases de datos

--- mercado.py
+++ mercado.py
@@ -1,12 +1,30 @@
+from enum import unique
+
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
 
 class Item(db.Model):
-    name = db.Column(db.String(length=32))
-    name = db.Column(db.String())
+    id = db.Column(db.Integer(),
+        primary_key=True
+    )
+    name = db.Column(db.String(length=32),
+        nullable=False,
+        unique=True
+    )
+    price = db.Column(db.Integer(),
+        nullable=False
+    )
+    barcode = db.Column(db.String(length=16),
+        nullable=False,
+        unique=True
+    )
+    description = db.Column(db.String(length=1024),
+        nullable=False
+    )
 
 @app.route("/")
 @app.route("/hogar")
