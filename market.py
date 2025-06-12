f183232e7cd88e26502f5377298e8676c95b128d
atommnava <atommunoznava@gmail.com>
2025-06-12 15:45:59 -0600

    Modelos y bases de datos

--- market.py
+++ market.py
@@ -1,10 +1,9 @@
-from enum import unique
-
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.app_context().push()
 db = SQLAlchemy(app)
 
 class Item(db.Model):
@@ -25,24 +24,12 @@ class Item(db.Model):
     description = db.Column(db.String(length=1024),
         nullable=False
     )
-
 @app.route("/")
 @app.route("/hogar")
 def home():
     return render_template('home.html')
 
-@app.route("/about/<usuario>")
-def about_page(usuario):
-    return f"<h1>Esta es la pagina ABOUT de {usuario}</h1>"
-
-@app.route("/mercado")
-def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Teclado', 'barcode': '231985128446', 'price': 150}
-    ]
-    return render_template('market.html', items=items)
+# ... resto de tus rutas ...
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
