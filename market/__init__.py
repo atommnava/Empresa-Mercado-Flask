12c52c25bf2e13e56a31a44751e8d27b5a141736
atommnava <atommunoznava@gmail.com>
2025-06-15 09:05:18 -0600

    Forms de Flask

--- market/__init__.py
+++ market/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.app_context().push()
+app.config['SECRET_KEY'] = '8cdd20bff06ff3be077b779f'
 db = SQLAlchemy(app)
 
 from market import routes
