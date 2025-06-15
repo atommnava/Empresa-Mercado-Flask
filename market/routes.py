4f116a4a0decbabda93492091d4ef04a7c6546ed
atommnava <atommunoznava@gmail.com>
2025-06-14 22:44:56 -0600

    Forms de Flask

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,8 @@
 from market import app
 from flask import render_template
 from market.models import Item
+from market.forms import RegisterForm
+
 @app.route('/')
 @app.route('/hogar')
 def home():
@@ -11,3 +13,7 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
