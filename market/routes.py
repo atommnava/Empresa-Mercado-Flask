2c5e75b4262bbf0414bcc366399ec0411206ed74
atommnava <atommunoznava@gmail.com>
2025-06-15 16:32:30 -0600

    Validaciones de Flask

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 @app.route('/')
 @app.route('/hogar')
@@ -16,4 +17,13 @@ def market_page():
 @app.route('/registro')
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(
+            username=form.username.data,
+            email_address=form.email_address.data,
+            password=form.password1.data
+        )
+        db.session.add(user_to_create)
+        db.session.commit()
+
     return render_template('register.html', form=form)
