3f5537e226ac636a5614a2b4d1c664445d13900d
atommnava <atommunoznava@gmail.com>
2025-06-19 15:36:31 -0600

    Autenticación de usuario 1

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
@@ -8,5 +9,6 @@ app.app_context().push()
 app.config['SECRET_KEY'] = '8cdd20bff06ff3be077b779f'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
