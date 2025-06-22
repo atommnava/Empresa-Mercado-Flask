63c02f5062950c601b5343cae00defe1e257559e
atommnava <atommunoznava@gmail.com>
2025-06-21 20:36:13 -0600

    Adquisición de Items (Funcionalidad 2)

--- market/routes.py
+++ market/routes.py
@@ -1,4 +1,4 @@
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from flask import render_template, redirect, url_for, flash, request
 from market import app
 from market.models import Item, User
@@ -14,10 +14,18 @@ def home_page():
 @login_required
 def market_page():
     purchase_form = PurchaseItemForm()
-    if purchase_form.validate_on_submit():
-        print(request.form.get('purchased_item'))
-    items = Item.query.all()
-    return render_template('market.html', items=items, purchase_form=purchase_form)
+    if request.method == "POST":
+        purchased_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            p_item_object.owner = current_user.id
+            current_user.budget -= p_item_object.price
+            db.session.commit()
+            flash(f"¡Tu Pago se realizo con Éxito! {p_item_object.name} por ${p_item_object.price}")
+
+    if request.method == "GET":
+        items = Item.query.filter_by(owner=None)
+        return render_template('market.html', items=items, purchase_form=purchase_form)
 
 @app.route('/registro', methods=['GET', 'POST'])
 def register_page():
