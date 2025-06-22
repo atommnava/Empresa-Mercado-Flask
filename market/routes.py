59fe1c73f297f62d5f2721e328395bbc60596045
atommnava <atommunoznava@gmail.com>
2025-06-22 11:42:46 -0600

    Vender Item V.FINAL

--- market/routes.py
+++ market/routes.py
@@ -2,7 +2,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 from flask import render_template, redirect, url_for, flash, request
 from market import app
 from market.models import Item, User
-from market.forms import RegisterForm, LoginForm, PurchaseItemForm
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
 from market import db
 
 @app.route('/')
@@ -14,6 +14,7 @@ def home_page():
 @login_required
 def market_page():
     purchase_form = PurchaseItemForm()
+    selling_form = SellItemForm()
     if request.method == "POST":
         purchased_item = request.form.get('purchased_item')
         p_item_object = Item.query.filter_by(name=purchased_item).first()
@@ -28,7 +29,7 @@ def market_page():
     if request.method == "GET":
         items = Item.query.filter_by(owner=None)
         owned_items = Item.query.filter_by(owner=current_user.id)
-        return render_template('market.html', items=items, purchase_form=purchase_form, owned_items=owned_items)
+        return render_template('market.html', items=items, purchase_form=purchase_form, owned_items=owned_items, selling_form=selling_form)
 
 @app.route('/registro', methods=['GET', 'POST'])
 def register_page():
